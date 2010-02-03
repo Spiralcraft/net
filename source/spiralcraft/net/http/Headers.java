1657a18b66d52a97bad863b45cca80970b7a5b01
mike <>
2010-02-03 10:09:26 +0000

    Moved http header constants into spiralcraft-net module Moved RangeHeader into spiralcraft-net module

---
+++ source/spiralcraft/net/http/Headers.java
@@ -0,0 +1,29 @@
+package spiralcraft.net.http;
+
+public class Headers
+{
+  
+  public final static String AUTHORIZATION = "Authorization"; 
+  public final static String CONNECTION = "Connection";
+  public final static String CONTENT_LENGTH = "Content-Length";
+  public final static String CONTENT_TYPE = "Content-Type";
+  public final static String CONTENT_ENCODING = "Content-Encoding";
+  public final static String COOKIE = "Cookie";
+  public final static String DATE = "Date"; 
+  public final static String HOST = "Host";
+  public final static String IF_MODIFIED_SINCE="If-Modified-Since";
+  public final static String IF_UNMODIFIED_SINCE="If-Unmodified-Since";
+  public final static String LOCATION = "Location";
+  public final static String LAST_MODIFIED = "Last-Modified";
+  public final static String REFERER="Referer";
+  public final static String SET_COOKIE = "Set-Cookie";
+  public final static String SERVER = "Server";
+  public final static String TRANSFER_ENCODING ="Transfer-Encoding";  
+  public final static String USER_AGENT="User-Agent";
+  public final static String WWW_AUTHENTICATE = "WWW-Authenticate"; 
+  public final static String CACHE_CONTROL="Cache-Control";
+  public final static String EXPIRES="Expires";
+  public final static String RANGE="Range";
+  public final static String CONTENT_RANGE="Content-Range";
+  
+}
